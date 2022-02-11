ed58691eaf35707758106980e40dca9a64491fca
SJGriffin21 <sjgriffin@quinnipiac.edu>
2022-02-11 11:24:54 -0500

    Divide by 0 Error

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,29 +12,36 @@ public class Calculator {
 		double two = input.nextDouble();
 		double answer = 0;
 		
+		
 		System.out.println("Do you want to add, subtract, multiply, or divide?");
 		String response = input.nextLine();
 		response = input.nextLine();
 		
 		if (response.equalsIgnoreCase("add")) {
 			answer = one + two;
+			System.out.print(answer);
 		}
 		
 		else if (response.equalsIgnoreCase("subtract")) {
 			answer = one - two;
+			System.out.print(answer);
 		}
 		
 		else if (response.equalsIgnoreCase("multiply")) {
 			answer = one * two;
+			System.out.print(answer);
 		}
 		
 		else if (response.equalsIgnoreCase("divide")) {
-			answer = one / two;
+			if (two == 0) {
+				System.out.println("Error: Divide by Zero");
+			}
+			else {
+				answer = one / two;
+				System.out.print(answer);
+			}
+				
 		}
-			
-		
-		
-		System.out.print(answer);
 		
 	}
 }
