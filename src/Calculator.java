ab598bdd70fcd5979f24664fd2e47f4fa0a9cb36
SJGriffin21 <sjgriffin@quinnipiac.edu>
2022-02-11 11:00:52 -0500

    Addition

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 
 public class Calculator {
 
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter two numbers on different lines to add them togethher");
+		
+		int one = input.nextInt();
+		int two = input.nextInt();
+		
+		int sum = one + two;
+		
+		System.out.print(sum);
+		
+	}
 }
